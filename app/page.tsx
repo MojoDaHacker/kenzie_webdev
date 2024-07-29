97e8985ef955818c41cc20c243329042253aef93
Matthew McKenzie <matthew.mckenzie@aa.com>
2024-07-28 23:48:34 -0700

    made video wawit for first load to attach event handler

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const Page = (props: Props) => {
   return (
-    <div className='w-full h-full flex justify-end items-end'>
+    <div className='w-full h-full flex justify-end items-end px-6'>
       <Navs />
     </div>
   )
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,32 +2,38 @@
 
 //@ts-nocheck
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Loader } from "rsuite";
 
 /**
  * Component for "Hero" Slices.
  */
 const Hero = ({ children, href }: any): JSX.Element => {
+    const [start, setStart] = useState(false)
     const [isVideoLoading, setVideoLoading] = useState(true)
+    const ref = useRef();
     const path = usePathname()
     const isPathHome = path == "/"
     const blurActive = isPathHome ? "" : "backdrop-blur"
     const brightnessActive = isPathHome ? "" : "brightness-50"
 
-    const handleVideoLoad = () => {console.log("asdasd");setVideoLoading(false)};
+    const handleVideoLoad = () => setVideoLoading(false);
+
+    useEffect(() => { setStart(true) }, [])
 
     return (
         <section
             // className="px-12"
-            style={{ height: "100vh" }}
+            style={{ height: "100vh", backgroundColor: "black" }}
         >
             <div style={{ width: "100%", height: "100%", position: "relative" }}>
                 <div className={`z-0 ${brightnessActive}`} style={{ width: "100%", height: "100%", position: "absolute" }}>
-                    <video playsInline onLoadedData={handleVideoLoad} muted autoPlay style={{ objectFit: "cover", display: "block", width: "100%", height: "100%", padding: 0, margin: 0 }} loop>
-                        <source src={href} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    {start && (
+                        <video playsInline onPlay={handleVideoLoad} onLoadedData={handleVideoLoad} preload="none" muted autoPlay style={{ objectFit: "cover", display: "block", width: "100%", height: "100%", padding: 0, margin: 0 }} loop>
+                            <source src="/exportedHeroVideo.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    )}
                 </div>
 
                 <div className={`h-full ${blurActive}  py-12`}>
